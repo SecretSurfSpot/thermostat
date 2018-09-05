d25e46b72e69ad297abe269f2d9f38eb6290b782
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-09-05 18:29:40 +0100

    refactor - add getCurrentTemperature, up & down

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,64 +1,30 @@
 'use strict'
 
-describe('thermostat', function(){
-  var thermostat
+describe('Thermostat', function(){
+  var thermostat;
 
   beforeEach(function(){
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function(){
-    expect(thermostat.temp).toEqual(20);
+  it('has a default temperature of 20', function(){
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('increases by 1 degree', function(){
-    thermostat.increaseTemp();
-    expect(thermostat.temp).toEqual(21);
+  it ('increases temperature with up()', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+    thermostat.up();
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(23);
   });
 
-  it('decreases by 1 degree', function(){
-    thermostat.decreaseTemp();
-    expect(thermostat.temp).toEqual(19);
+  it ('decreases temperature with down()', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+    thermostat.down();
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(17);
   });
-
-  it('has a minimum temp of 10 degrees', function() {
-    thermostat.temp = 10;
-    expect(function(){ thermostat.decreaseTemp(); }).toThrowError('this is already the min temp');
-  });
-
-  it('has a max temp of 25 if power saving mode is on', function(){
-    thermostat.temp = 25;
-    spyOn(thermostat,'isPowerSaving').and.returnValue(true);
-    expect(function(){ thermostat.increaseTemp(); }).toThrowError('power saving on 25 degrees is the max temp');
-  });
-
-  it('has a max temp of 32 degrees if power saving mode is off', function(){
-    thermostat.temp = 32;
-  //  spyOn(thermostat,'isPowerSaving').and.returnValue(false);
-    expect(function(){ thermostat.increaseTemp(); }).toThrowError('this is the max temp') ;
-  });
-
-  it('has a temp of 20 degrees if reset option chosen', function(){
-    thermostat.reset();
-    expect(thermostat.temp).toEqual(20);
-  });
-
-  describe('energy usage', function(){
-    it('has current energy usage of low-usage', function(){
-      thermostat.temp = 15
-      expect(thermostat.energyUsage(thermostat.temp)).toEqual('low-usage');
-    });
-
-    it('has current energy usage of medium-usage', function(){
-      thermostat.temp = 21
-      expect(thermostat.energyUsage(thermostat.temp)).toEqual('medium-usage');
-    });
-
-    it('has current energy usage of high-usage', function(){
-      thermostat.temp = 31
-      expect(thermostat.energyUsage(thermostat.temp)).toEqual('high-usage');
-    });
-  });
-
 
 });
